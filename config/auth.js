cb593bcc2b2c8f2340c31a0cbb58a47fca4af253
Gancho Danailov <gancho.danailov@modacom.com>
2015-08-29 01:53:24 +0300

    add facebook login functionality

--- config/auth.js
+++ config/auth.js
@@ -4,21 +4,21 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1142455015764546', // your App ID
-        'clientSecret'  : 'fc2b3077f29c77102b7c4a7c8408669c', // your App Secret
-        'callbackURL'   : 'https://x-list.herokuapp.com/auth/facebook/callback'
+        'clientID'        : '1609649682633897', // your App ID
+        'clientSecret'    : 'bc42cb36032c054fcaa7a352d91d5966', // your App Secret
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'        : 'your-consumer-key-here',
+        'consumerSecret'     : 'your-client-secret-here',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
-        'clientID'      : 'your-secret-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'clientID'         : '965337704175-r5ep86tntlod1h7mmoembc0hpso8a0na.apps.googleusercontent.com',
+        'clientSecret'     : 'jkHdyPZ9F7OEmlWWX53PBqhT',
+        'callbackURL'      : 'http://127.0.0.1:8080/auth/google/callback'
     }
 
 };
--- server.js
+++ server.js
@@ -17,30 +17,24 @@ var session      = require('express-session');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
- // connect to our database
-mongoose.connect(configDB.url, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
+mongoose.connect(configDB.url); // connect to our database
+
 require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'xlist' })); // session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
